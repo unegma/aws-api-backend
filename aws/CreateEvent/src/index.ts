79c08c253c27620f3498143c3812f857b8d4d31a
Timothy Coleman <timhc22@gmail.com>
2022-07-18 08:40:05 +0100

    Add example data

--- aws/CreateEvent/src/index.ts
+++ aws/CreateEvent/src/index.ts
@@ -26,6 +26,7 @@ export const handler = async (event: APIGatewayProxyEvent, context: any = {}): P
 
   let message = "# Success";
   try {
+    // @ts-ignore
     let origin = event.headers.origin;
     console.log(`Origin: ${origin}`);
 
@@ -36,6 +37,12 @@ export const handler = async (event: APIGatewayProxyEvent, context: any = {}): P
     ) {
       throw new Error('Unauthorized');
     }
+
+
+    // @ts-ignore
+    let data = JSON.parse(event.body);
+    console.log(`Data: ${JSON.stringify(data)}`);
+
 
     // let data = await dbUtilities.getFromDB(AWS_LAMBDA_FUNCTION_NAME, 'UserAndKey', null, '');
     // console.log(data);
@@ -53,6 +60,55 @@ export const handler = async (event: APIGatewayProxyEvent, context: any = {}): P
     //
     // console.log(data);
 
+
+
+
+    // Refer to the Node.js quickstart on how to setup the environment:
+    // https://developers.google.com/calendar/quickstart/node
+    // Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+    // stored credentials.
+
+    let eventData = {
+      'summary': data.summary,
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2015-05-28T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'dateTime': '2015-05-28T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+      ],
+      'reminders': {
+        'useDefault': false,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    };
+
+    // calendar.events.insert({
+    //   auth: auth,
+    //   calendarId: 'primary',
+    //   resource: event,
+    // }, function(err, event) {
+    //   if (err) {
+    //     console.log('There was an error contacting the Calendar service: ' + err);
+    //     return;
+    //   }
+    //   console.log('Event created: %s', event.htmlLink);
+    // });
+
+    let message = eventData;
 
 
   } catch(error: any) {
