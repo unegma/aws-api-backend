91f8146de2236f95010654c9912d0ecc2fb8d365
Timothy Coleman <timhc22@gmail.com>
2022-08-09 15:18:32 +0100

    Add more calendar data

--- aws/CreateEvent/src/index.ts
+++ aws/CreateEvent/src/index.ts
@@ -83,6 +83,118 @@ export const handler = async (event: APIGatewayProxyEvent, context: any = {}): P
     // stored credentials.
 
 
+    // type EventData = {
+    //   summary: string,
+    //   location?:
+    //   description: string,
+    //   start: {
+    //     dateTime: string // '2015-05-28T09:00:00-07:00'
+    //     timeZone: string // 'America/Los_Angeles'
+    //   },
+    //   end: {
+    //     dateTime: string,
+    //     timeZone: string
+    //   }
+    //   attendees: [
+    //     { email: string}
+    //   ]
+    // }
+
+    // let eventData = {
+    //   'summary': data.summary,
+    //   'location': '800 Howard St., San Francisco, CA 94103',
+    //   'description': 'A chance to hear more about Google\'s developer products.',
+    //   'start': {
+    //     'dateTime': '2015-05-28T09:00:00-07:00',
+    //     'timeZone': 'America/Los_Angeles',
+    //   },
+    //   'end': {
+    //     'dateTime': '2015-05-28T17:00:00-07:00',
+    //     'timeZone': 'America/Los_Angeles',
+    //   },
+    //   'recurrence': [
+    //     'RRULE:FREQ=DAILY;COUNT=2'
+    //   ],
+    //   'attendees': [
+    //     {'email': 'lpage@example.com'},
+    //     {'email': 'sbrin@example.com'},
+    //   ],
+    //   'reminders': {
+    //     'useDefault': false,
+    //     'overrides': [
+    //       {'method': 'email', 'minutes': 24 * 60},
+    //       {'method': 'popup', 'minutes': 10},
+    //     ],
+    //   },
+    // };
+    //
+    // let eventData = {
+    //   'summary': data.summary,
+    //   'location': '800 Howard St., San Francisco, CA 94103',
+    //   'description': data.description,
+    //   'start': {
+    //     'dateTime': data.start.dateTime,
+    //     'timeZone': data.start.timeZone,
+    //   },
+    //   'end': {
+    //     'dateTime': data.end.dateTime,
+    //     'timeZone': data.end.timeZone,
+    //   },
+    //   'recurrence': [
+    //     'RRULE:FREQ=DAILY;COUNT=2'
+    //   ],
+    //   'attendees': [
+    //     data.attendees
+    //   ],
+    //   'reminders': {
+    //     'useDefault': false,
+    //     'overrides': [
+    //       {'method': 'email', 'minutes': 24 * 60},
+    //       {'method': 'popup', 'minutes': 10},
+    //     ],
+    //   },
+    // } ;
+
+
+    // calendar.events.insert({
+    //   auth: auth,
+    //   calendarId: 'primary',
+    //   resource: eventData,
+    // }, function(err, eventData) {
+    //   if (err) {
+    //     console.log('There was an error contacting the Calendar service: ' + err);
+    //     return;
+    //   }
+    //   console.log('Event created: %s', eventData.htmlLink);
+    // });
+
+
+
+
+
+    // let message = eventData;
+    //
+    //
+    // const calendar = google.calendar({version: 'v3', auth});
+    // calendar.events.list({
+    //   calendarId: 'primary',
+    //   timeMin: (new Date()).toISOString(),
+    //   maxResults: 10,
+    //   singleEvents: true,
+    //   orderBy: 'startTime',
+    // }, (err, res) => {
+    //   if (err) return console.log('The API returned an error: ' + err);
+    //   const events = res.data.items;
+    //   if (events.length) {
+    //     console.log('Upcoming 10 events:');
+    //     events.map((event, i) => {
+    //       const start = event.start.dateTime || event.start.date;
+    //       console.log(`${start} - ${event.summary}`);
+    //     });
+    //   } else {
+    //     console.log('No upcoming events found.');
+    //   }
+    // });
 
     console.log(`Completed ${AWS_LAMBDA_FUNCTION_NAME}`);
 
