701c0f60f8670b3ae44140294842c0cf37bf17e6
Timothy Coleman <timhc22@gmail.com>
2022-12-11 03:56:46 +0000

    Start adding terraform

---
+++ aws/SendMessage/terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+#      version = "~> 3.27"
+    }
+  }
+
+#  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "unegma"
+  region = "eu-west-2"
+  max_retries = 1
+}
