6df187bc95d21e0b6c8af4c515302a4fe3d3a627
Timothy Coleman <timhc22@gmail.com>
2022-12-12 01:42:05 +0000

    Add todo

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,6 +6,7 @@ cd dist
 # bump version without creating ugly commits: https://stackoverflow.com/questions/66717190/is-there-a-way-to-bump-the-version-without-version-commits
 npm version --commit-hooks false --git-tag-version false minor
 
+#todo fix function.zip not being deleted
 echo "## Cleaning up..."
 rm ./function.zip
 
